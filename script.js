2acc89c9dd605e2376b49be5911be775ba5058c4
LeraDolgova109 <Dol0914@yandex.ru>
2025-01-24 15:48:10 +0700

    Добавление возможности вывода нажатых чисел на экран

---
+++ script.js
@@ -0,0 +1,13 @@
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = currentOperand;
+}
