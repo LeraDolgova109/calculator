2700b093e5dbd9f96f2aac2b3489de515a4ca066
LeraDolgova109 <Dol0914@yandex.ru>
2025-01-24 15:53:23 +0700

    Добавление логики для кнопок с операциями: проведение вычислений

--- script.js
+++ script.js
@@ -8,6 +8,43 @@ function appendNumber(number) {
     updateDisplay();
 }
 
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        calculate();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function calculate() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
 function updateDisplay() {
     document.getElementById('display').value = currentOperand;
 }
